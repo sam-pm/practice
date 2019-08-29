6c9bde5831b267b40935e959376bf022f1fa14a1
spayton <spayton@prometheusgroup.com>
2019-08-29 16:13:53 -0400

    updated with partial solution for Q1

--- Q1.py
+++ Q1.py
@@ -1,1 +1,7 @@
-print("First commit")
+class Solution:
+	def twoSum(self, nums, target):
+		index = 0
+		while(index < len(nums)):
+			if (target - nums[index] in nums):
+				#then we want to add index and the index of the other number
+				
