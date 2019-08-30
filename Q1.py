63611cdb9806b9588467aac56f7f263586b47713
spayton <spayton@prometheusgroup.com>
2019-08-30 10:32:06 -0400

    Final answer for Q1 - Python

--- Q1.py
+++ Q1.py
@@ -1,7 +1,11 @@
 class Solution:
 	def twoSum(self, nums, target):
-		index = 0
-		while(index < len(nums)):
-			if (target - nums[index] in nums):
-				#then we want to add index and the index of the other number
-				
+		#print("The target number is: "+str(target))
+		answer = []
+		for x in nums:
+			if ((target - x) in nums and (target-x) != x):
+				answer.append(nums.index(x))
+				answer.append(nums.index(target-x))
+				print(answer)
+				break
+		return answer
