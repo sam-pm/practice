72f96edf6bee7d6c7abafe86f2c053bfe762d992
spayton <spayton@prometheusgroup.com>
2019-09-03 16:48:08 -0400

    Final solution for Q1 in Java

--- Q1.java
+++ Q1.java
@@ -1,11 +1,26 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Q1{
 
     public int[] twoSum(int[] nums, int target){
-      System.out.println("The value of target is: " + target);
-      System.out.println("The value of nums is: " + Arrays.toString(nums));
-      return null;
+
+      // Data structures needed for solution
+      HashMap<Integer, Integer> map = new HashMap<>();
+      int[] solution = new int[2];
+
+      //Code to figure out what the solution is.
+      for(int i = 0; i < nums.length; i++){
+        if(map.containsKey(nums[i])){
+          solution[0] = map.get(nums[i]);
+          solution[1] = i;
+        }else{
+          map.put(target-nums[i],i);
+        }
+      }
+
+      return solution;
     }
 
 
