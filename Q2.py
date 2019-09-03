088c38af34fca0c296aafab9d96f7e490882411f
spayton <spayton@prometheusgroup.com>
2019-09-03 17:28:46 -0400

    Commiting before heading out of work

--- Q2.py
+++ Q2.py
@@ -1,7 +1,13 @@
 class Solution:
     def addTwoNumbers(self, list1, list2):
-        
+
+        temp_l1 = list1
         # Will need to iterate through the link list node
-        while (temp_node != None):
-            print(str(temp_node.val))
-            temp_node = temp_node.next
+        while (temp_l1 != None):
+            print(str(temp_l1.val))
+            temp_l1 = temp_l1.next
+
+        temp_l2 = list2
+        while (temp_l2 != None):
+            print(str(temp_l2.val))
+            temp_l2 = temp_l2.next
