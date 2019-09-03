01780cecf7c2fbd188eaa63f931ff5cb193665a8
spayton <spayton@prometheusgroup.com>
2019-09-03 15:47:51 -0400

    Re-doing Java answer

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main{
+
+    public static void main(String[] args){
+
+      Q1_solution s = new Q1_solution();
+      int[] numList = new int[]{3,8,4,12,2,10};
+      s.twoSum(numList, 15);
+
+    }
+}
