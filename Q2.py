317087949b37b68885cd26193ce73f8186d0e516
spayton <spayton@prometheusgroup.com>
2019-09-03 17:20:54 -0400

    Starting solution to Q2

---
+++ Q2.py
@@ -0,0 +1,7 @@
+class Solution:
+    def addTwoNumbers(self, list1, list2):
+        
+        # Will need to iterate through the link list node
+        while (temp_node != None):
+            print(str(temp_node.val))
+            temp_node = temp_node.next
