3ce1c12d5270fefd46b790174226b7412bc8afe2
spayton <spayton@prometheusgroup.com>
2019-09-04 16:46:54 -0400

    Final solution for Q2 -- Python

--- Q2.py
+++ Q2.py
@@ -1,13 +1,41 @@
+from ListNode import ListNode
+
 class Solution:
     def addTwoNumbers(self, list1, list2):
 
-        temp_l1 = list1
-        # Will need to iterate through the link list node
-        while (temp_l1 != None):
-            print(str(temp_l1.val))
-            temp_l1 = temp_l1.next
+        # Collect the values in a list and flip it.
+        str_num_1 = []
+        while (list1 != None):
+            str_num_1.append(str(list1.val))
+            list1 = list1.next
+        str_num_1.reverse()
 
-        temp_l2 = list2
-        while (temp_l2 != None):
-            print(str(temp_l2.val))
-            temp_l2 = temp_l2.next
+        str_num_2 = []
+        while (list2 != None):
+            str_num_2.append(str(list2.val))
+            list2 = list2.next
+        str_num_2.reverse()
+
+        # Combine and add
+        str_num_1 = ''.join(str_num_1)
+        str_num_2 = ''.join(str_num_2)
+        added_nums = str(int(str_num_1)+int(str_num_2))
+
+        # Flip anwser and return
+        added_nums = added_nums[::-1]
+        answer_list = ListNode(0)
+        temp_node = answer_list
+        for i, c in enumerate(added_nums):
+            temp_node.val = c
+            if i < len(added_nums)-1:
+                temp_node.next = ListNode(0)
+            temp_node = temp_node.next
+
+
+
+        # while (answer_list != None):
+        #     if answer_list.next == None:
+        #         print(str(answer_list.val), end='')
+        #     else:
+        #         print(str(answer_list.val)+ " => ", end='')
+        #     answer_list = answer_list.next
