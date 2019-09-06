25b0a0a8d33ee155b83a20d18bb7e0101b2cd246
spayton <spayton@prometheusgroup.com>
2019-09-06 08:25:01 -0400

    Updating git before starting updates

---
+++ ListNode.java
@@ -0,0 +1,10 @@
+public class ListNode{
+  // instance variables
+  int val;
+  ListNode next;
+
+  // Constructer
+  public ListNode(int val){
+    this.val = val;
+  }
+}
---
+++ Q2.java
@@ -0,0 +1,16 @@
+
+public class Q2{
+
+	// Default constructer is used for this class
+
+	// Solution function
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+		System.out.println("made it");
+		System.out.println(l1.val);
+		System.out.println(l2.val);
+
+		return null;
+	}
+
+
+}
