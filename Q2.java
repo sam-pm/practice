3229b1e9379c1de7038f9021be75661ccf273fb2
spayton <spayton@prometheusgroup.com>
2019-09-06 11:27:00 -0400

    progress on solution file

--- Q2.java
+++ Q2.java
@@ -5,11 +5,36 @@ public class Q2{
 
 	// Solution function
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2){
-		System.out.println("made it");
-		System.out.println(l1.val);
-		System.out.println(l2.val);
 
-		return null;
+	    System.out.println(l1.val);
+	    System.out.println(l2.val);
+
+	    //This is the point that the nodes need to be processed.
+	    String temp_1 = "";
+	    while(l1 != null){
+		// Append the val to a string
+		temp_1.concat(String.valueOf(l1.val));
+		
+		l1 = l1.next;
+	    }
+
+	    System.out.println("The value of the first appended string is: " + temp_1);
+
+	    String temp_2 = "";
+	    while (l2 != null){
+		// Append the val to a string
+		temp_2.concat(String.valueOf(l2.val));
+
+		
+		l2 = l2.next;
+	    }
+
+	    System.out.println("The value of the second appended string is: " + temp_2);
+
+	    // add the numbers and convert the result to a reversed Node string
+
+	    // Return answer Node
+	    return null;
 	}
 
 
