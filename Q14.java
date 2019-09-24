29c654bbe3a873b2f843bd3a517240851f0dadd0
spayton <spayton@prometheusgroup.com>
2019-09-24 17:27:57 -0400

    End of day commit. Q13 complete, Q14 in progress

---
+++ Q14.java
@@ -0,0 +1,16 @@
+public class Q14{
+    public String longestCommonPrefix(String[] strs){
+	// variables needed
+	String longest;
+
+	for(String str : strs){
+	    if (str.length() > longest.length()){
+		// stub
+	    }
+	}
+
+
+	
+	return null;
+    }
+}
