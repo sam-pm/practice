02489ee0f28553d7af9d4b693709554df29a99ea
spayton <spayton@prometheusgroup.com>
2019-09-25 14:52:02 -0400

    Java solution with results

--- Q14/Q14.java
+++ Q14/Q14.java
@@ -1,16 +1,29 @@
 public class Q14{
     public String longestCommonPrefix(String[] strs){
 	// variables needed
-	String longest;
+	String longest = strs.length > 0 ? strs[0] : "";
 
-	for(String str : strs){
-	    if (str.length() > longest.length()){
-		// stub
+	for(int i = 0; i < strs.length; i++){
+	    // Scope variables
+	    String curStr = "";
+	    int sm_str = strs[i].length() > longest.length() ? longest.length() : strs[i].length();
+
+	    for(int j = 0; j < sm_str; j++){
+		if(longest.charAt(j) == strs[i].charAt(j)){
+		    curStr = curStr + strs[i].charAt(j);
+		}else {
+		    break;
+		}
 	    }
+	    longest = curStr;
 	}
 
-
-	
-	return null;
+	return longest;
     }
 }
+
+/*
+Runtime: 6 ms, faster than 6.66% of Java online submissions for Longest Common Prefix.
+Memory Usage: 36.7 MB, less than 99.42% of Java online submissions for Longest Common Prefix.
+*/
+
--- Q14/driver14.java
+++ Q14/driver14.java
@@ -1,17 +1,32 @@
+import java.util.Arrays;
+
 public class driver14{
     public static void main(String[] args){
+	// Variables and objects
 	Q14 s = new Q14();
-	int t1, t2;
-	t1 = t2 = 0;
-	
-	// Tests
-	
-         
+	int tr = 0;
+	String[] s1 = {"terst", "terrior", "terrific"}; // matches "ter"
+	String[] s2 = {"no", "matching", "words"}; // no matches
+	String[] s3 = {"what"}; // only one entry
+	String[] s4 = {"boom", "boom", "boom"};
+	String[] s5 = {};
+
+	// Tests    
+	if(s.longestCommonPrefix(s1).compareTo("ter") == 0) tr++;
+	if(s.longestCommonPrefix(s2).compareTo("") == 0) tr++;
+	if(s.longestCommonPrefix(s3).compareTo("what") == 0) tr++;
+	if(s.longestCommonPrefix(s4).compareTo("boom") == 0) tr++;
+	if(s.longestCommonPrefix(s5).compareTo("") == 0) tr++;
+
 
 	// Test results
 	System.out.println("Test Results:");
-	System.out.println("Valid Tests passed: " + t1);
-	System.out.println("Invalid Tests Passed: " + t2);
+	System.out.println("Test 1: String array = " + Arrays.toString(s1) + " | expected result: \"ter\" | actual result: \"" + s.longestCommonPrefix(s1) + "\"");
+	System.out.println("Test 2: String array = " + Arrays.toString(s2) + " | expected result: \"\" | actual result: \"" + s.longestCommonPrefix(s2) + "\"");
+	System.out.println("Test 3: String array = " + Arrays.toString(s3) + " | expected result: \"what\" | actual result: \"" + s.longestCommonPrefix(s3) + "\"");
+	System.out.println("Test 4: String array = " + Arrays.toString(s4) + " | expected result: \"boom\" | actual result: \"" + s.longestCommonPrefix(s4) + "\"");
+	System.out.println("Test 5: String array = " + Arrays.toString(s5) + " | expected result: \"\" | actual result: \"" + s.longestCommonPrefix(s5) + "\"");
+	System.out.println("Test cases passed: " + tr + "/5");
 	
     }
 }
