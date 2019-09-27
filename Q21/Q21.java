2c4cb599eab3044c8b7096320d34b0bc691309d6
spayton <spayton@prometheusgroup.com>
2019-09-27 10:22:53 -0400

    commiting last changes before weekend

--- Q21/Q21.java
+++ Q21/Q21.java
@@ -1,6 +1,20 @@
 public class Q21{
     public ListNode mergeTwoLists(ListNode l1, ListNode l2){
 
+	System.out.println("List one values");
+	while(l1 != null){
+	    System.out.print(l1.val + " -> ");
+	    l1 = l1.next;
+	}
+
+	System.out.println("\nList two values");
+	while(l2 != null){
+	    System.out.print(l2.val + " -> ");
+	    l2 = l2.next;
+	}
+
+	System.out.println("\n");
+	
 	return null;
     }
 }
--- Q21/driver21.java
+++ Q21/driver21.java
@@ -1,25 +1,61 @@
 import java.util.Random;
+import java.util.Arrays;
 
 public class driver21{
     public static void main(String[] args){
 	// Create and populate variables
 	Q21 s = new Q21();
 	int tr = 0;
-	ListNode l1, l2, l3, l4;
+	ListNode l1, l2, l3, l4, t1, t2, t3, t4;
+	Random r = new Random();
+	int[] i1 = new int[3];
+	int[] i2 = new int[3];
+	int[] i3 = new int[5];
+	int[] i4 = new int[5];
 
-	for (int i = 0; i < 4; i++){
-	    // stub for l1 and l2
+	for(int i = 0; i < 3; i++){
+	    i1[i] = r.nextInt(99);
+	    i2[i] = r.nextInt(99);
+	}
+	for(int i = 0; i < 5; i++){
+	    i3[i] = r.nextInt(99);
+	    i4[i] = r.nextInt(99);
+	}
+	// Sort the lists
+	Arrays.sort(i1);
+	Arrays.sort(i2);
+	Arrays.sort(i3);
+	Arrays.sort(i4);
+	
+	l1 = new ListNode(i1[0]);
+	l2 = new ListNode(i2[0]);
+	t1 = l1;
+	t2 = l2;
+	for (int i = 1; i < i1.length; i++){
+	    t1.next = new ListNode(i1[i]);
+	    t2.next = new ListNode(i2[i]);
+	    t1 = t1.next;
+	    t2 = t2.next;
 	}
 
-	for (int i = 0; i < 6; i++){
-	    // Stub for l3 and l4
+	l3 = new ListNode(i3[0]);
+	l4 = new ListNode(i4[0]);
+	t3 = l3;
+	t4 = l4;
+	for (int i = 1; i < i3.length; i++){
+	    t3.next = new ListNode(i3[i]);
+	    t4.next = new ListNode(i4[i]);
+	    t3 = t3.next;
+	    t4 = t4.next;
 	}
 
 	// Tests
-
+	if(s.mergeTwoLists(l1,l2) != null) tr++;
+	if(s.mergeTwoLists(l3,l4) != null) tr++;
 
 	// Results
-	
+	System.out.println("Testing Results");
+	System.out.println(tr + "/2 tests passed");
 	
     }
     
